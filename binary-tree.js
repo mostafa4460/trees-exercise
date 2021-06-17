f5efc179dcaebe9266afc605ec52eb1793997d5b
Mostafa Nassr <mostafa.nassr88@gmail.com>
2021-06-17 10:36:08 -0400

    Binary Trees

--- binary-tree.js
+++ binary-tree.js
@@ -17,28 +17,99 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth() {
+    let count = 0;
+    if (!this.root) return count;
 
+    let queueToVisit = [this.root];
+    while (queueToVisit.length) {
+      let currNode = queueToVisit.shift();
+      if (!currNode.val) return count;
+      else if (!(currNode.left || currNode.right)) return count + 1;
+      queueToVisit.push(currNode.left);
+      queueToVisit.push(currNode.right);
+      count += 1;
+    }
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
   maxDepth() {
+    if (!this.root) return 0;
+    let count = 1;
 
+    let queueToVisit = [this.root];
+    while (queueToVisit.length) {
+      let currNode = queueToVisit.shift();
+      if (currNode.left || currNode.right) {
+        count += 1;
+        if (currNode.left) queueToVisit.push(currNode.left);
+        if (currNode.right) queueToVisit.push(currNode.right);
+      }
+    }
+    return count;
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
+    if (!this.root) return 0;
+    let sum = this.root.val;
 
+    let rightSide = this.root.right,
+      rightSideRightSum = 0;
+    while (rightSide) {
+      rightSideRightSum += rightSide.val;
+      rightSide = rightSide.right;
+    }
+
+    rightSide = this.root.right;
+    let rightSideLeftSum = 0;
+    while (rightSide) {
+      rightSideLeftSum += rightSide.val;
+      rightSide = rightSide.left;
+    }
+
+    if (rightSideRightSum >= rightSideLeftSum) sum += rightSideRightSum;
+    else sum += rightSideLeftSum;
+
+    let leftSide = this.root.left,
+      leftSideRightSum = 0;
+    while (leftSide) {
+      leftSideRightSum += leftSide.val;
+      leftSide = leftSide.right;
+    }
+
+    leftSide = this.root.left;
+    let leftSideLeftSum = 0;
+    while (leftSide) {
+      leftSideLeftSum += leftSide.val;
+      leftSide = leftSide.left;
+    }
+
+    if (leftSideRightSum >= leftSideLeftSum) sum += leftSideRightSum;
+    else sum += leftSideLeftSum;
+
+    return sum;
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound) {
+    if (!this.root) return null;
+    let foundVals = [],
+      stackToVisit = [this.root];
 
+    while (stackToVisit.length) {
+      let currNode = stackToVisit.pop();
+      if (currNode.val > lowerBound) foundVals.push(currNode.val);
+      if (currNode.right) stackToVisit.push(currNode.right);
+      if (currNode.left) stackToVisit.push(currNode.left);
+    }
+    if (!foundVals.length) return null;
+    else return Math.min(...foundVals);
   }
 
   /** Further study!
